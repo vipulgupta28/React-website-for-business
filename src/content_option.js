12b9737c642e200c2dea0499324216a17644b9c2
Vipul Gupta <121814604+vipulgupta28@users.noreply.github.com>
2025-01-06 22:03:59 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,210 @@
+const logotext = "Calcutta Printers";
+const meta = {
+  title: "Calcutta Printers",
+  description:
+    "We Are Calcutta Printers _ We expertise in Printing of every Kind. You name it we have it",
+};
+const introdata = {
+  title: "We Are Calcutta Printers",
+  animated: {
+    first: "We Print on Demand",
+    second: "We Print Quality and Quantity",
+    third: "We Deliver Creativity at Your Fingertips",
+  },
+  description:
+    "At Calcutta Printers, we combine passion, precision, and creativity to bring your ideas to life. Whether it’s custom designs, large-scale printing, or on-demand solutions, we pride ourselves on delivering unparalleled quality and excellence. From vibrant marketing materials to stunning personalized prints, we ensure every detail is crafted to perfection. With state-of-the-art technology and a commitment to customer satisfaction, we’re your trusted partner for all your printing needs. Let’s create something extraordinary together.\nAddress: H-1084 Keshav Vatika Road, Kanpur Kalyanpur",
+  your_img_url:
+    "https://images.unsplash.com/photo-1503694978374-8a2fa686963a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+  title: "A Bit About Us",
+  aboutme:
+    "We are an all-in-one printing solutions provider, specializing in a wide range of services to meet all your printing needs. From commercial printing and personalized products to large-scale banners and promotional materials, we do it all. Whether you need high-quality business cards, brochures, custom packaging, or eye-catching signage, our team ensures impeccable results with state-of-the-art technology and innovative techniques. At Calcutta Printers, we focus on precision, creativity, and delivering unmatched customer satisfaction. No matter the scale or complexity, we turn your ideas into beautifully printed realities.",
+};
+
+const worktimeline = [
+  {
+    jobtitle: "Printed 1000 Cards for IIT Kanpur Fest",
+
+    date: "2024",
+  },
+  {
+    jobtitle: "Made 10 Banners for Advertisements",
+
+    date: "2019",
+  },
+  {
+    jobtitle: "Printed Cards for Businesses",
+
+    date: "2019",
+  },
+];
+
+const skills = [
+  // {
+  //   name: "Python",
+  //   value: 90,
+  // },
+  // {
+  //   name: "Djano",
+  //   value: 85,
+  // },
+  // {
+  //   name: "Javascript",
+  //   value: 80,
+  // },
+  // {
+  //   name: "React",
+  //   value: 60,
+  // },
+  // {
+  //   name: "Jquery",
+  //   value: 85,
+  // },
+];
+
+const services = [
+  {
+    title: "Commercial Printing Services",
+    description:
+      "Providing high-quality printing solutions for business needs, including business cards, brochures, flyers, posters, catalogs, and branded stationery like letterheads and envelopes.",
+  },
+  {
+    title: "Digital Printing Services",
+    description:
+      "Fast and cost-effective on-demand printing with options for personalized materials such as invitations, mailers, photobooks, and short-run prints with vibrant colors and sharp details.",
+  },
+  {
+    title: "Offset Printing",
+    description:
+      "Ideal for large-volume printing with superior quality and cost efficiency, suitable for books, magazines, newspapers, and packaging materials. Ensures consistent results across all prints.",
+  },
+  {
+    title: "Large-Format Printing",
+    description:
+      "Creating impactful visuals with large-format prints like banners, signage, wall murals, vinyl decals, and posters. Perfect for events, exhibitions, and outdoor advertising.",
+  },
+  {
+    title: "Packaging Solutions",
+    description:
+      "Custom packaging designs including labels, boxes, paper bags, and gift wraps that enhance brand identity while ensuring durability and aesthetic appeal.",
+  },
+  {
+    title: "Specialized Printing",
+    description:
+      "Offering advanced printing techniques such as embossing, debossing, foil stamping, die-cutting, and spot UV coating to add unique finishes and elevate the design of your materials.",
+  },
+  {
+    title: "Publishing Services",
+    description:
+      "Comprehensive book printing services, including hardback, paperback, and custom binding. Suitable for educational materials, novels, coffee table books, and regular publications like newspapers and magazines.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://images.unsplash.com/photo-1606836483372-d59cfc76228f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1661963087020-9f05138f9cce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://media.istockphoto.com/id/95339145/photo/close-up-of-a-printing-machine.jpg?s=612x612&w=0&k=20&c=XuKKyNPFE8g5-AbvoQ018EcsxsBJfFql_T0v90w5aAI=",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://media.istockphoto.com/id/1127947543/photo/printing-newspapers.webp?a=1&b=1&s=612x612&w=0&k=20&c=349tQeSz1IZkZKOfLSzTB3KkQtBnM8A94zDj7YmdQbg=",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://www.chilliprinting.com/Online-Printing-Blog/wp-content/uploads/2016/09/bulk-poster-prints-chilliprinting.jpg",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://hobancards.com/cdn/shop/products/original_paul_allen_1600_900x.jpg?v=1669496002",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://printdrs.com/wp-content/uploads/2020/06/Paper-for-Poster-1080x675.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "calcuttaprinters@gmail.com",
+  YOUR_FONE: "9889162992 , 8840972644 ",
+  ADDRESS: "H-1084 Keshav Vatika Road, Kalyanpur,Kanpur",
+  description:
+    "We would love to hear from you! Feel free to reach out to us using the contact details provided above. Whether you need high-quality prints, creative designs, or customized solutions for your business or personal needs, our team is here to bring your ideas to life with precision and care. Let us help you make an impactful impression with our exceptional printing services  ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  facebook: "https://facebook.com",
+  Instagram: "https://instagram.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
